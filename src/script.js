15ba4f6cd9a9a95ee5bb03e414e6a1aede0631bd
nclaire29 <nastasia.claire89@gmail.com>
2021-07-30 01:43:20 -0400

    Updated day and time with minutes and hours edits

--- src/script.js
+++ src/script.js
@@ -1,3 +1,30 @@
+function formatDate(timestamp){ //timestamp is the # of milliseconds since 1/19/70
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [ //create array so that name of day shows instead of index
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[date.getDay()]; //set day = array[function.getDay()];
+
+    
+    return `${day} ${hours}:${minutes}`;
+}
+
+
+
 function displayTemperature(response){ //function activates when we get response from api
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +37,8 @@ function displayTemperature(response){ //function activates when we get response
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000); //send timestamp (inside response.data.dt *1000) to formatDate function so it is reflects in innerHTML
 
 }
 
