4f1d35d0f76ad75d8636f34a964ed31d7857d4d6
nclaire29 <nastasia.claire89@gmail.com>
2021-08-01 12:59:15 -0400

    Updated img icon to match what is inside api

--- src/script.js
+++ src/script.js
@@ -39,11 +39,14 @@ function displayTemperature(response){ //function activates when we get response
     windElement.innerHTML = Math.round(response.data.wind.speed);
     let dateElement = document.querySelector("#date");
     dateElement.innerHTML = formatDate(response.data.dt * 1000); //send timestamp (inside response.data.dt *1000) to formatDate function so it is reflects in innerHTML
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
 
 let apiKey = "ab6da5069e5bc23122a387b3e99bd05b";
-let city = "Munich";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
