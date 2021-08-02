5455b941e860b293a43236505e95d0f449f277ba
nclaire29 <nastasia.claire89@gmail.com>
2021-08-01 21:22:14 -0400

    Add form in HTML - create functions search and handleSubmit

--- src/script.js
+++ src/script.js
@@ -45,9 +45,20 @@ function displayTemperature(response){ //function activates when we get response
 
 }
 
-let apiKey = "ab6da5069e5bc23122a387b3e99bd05b";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey = "ab6da5069e5bc23122a387b3e99bd05b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Chicago");
+
+
+let formElement = document.querySelector("#search-form");
+formElement.addEventListener("submit", handleSubmit);
